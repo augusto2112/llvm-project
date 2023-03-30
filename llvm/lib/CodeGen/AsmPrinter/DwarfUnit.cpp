115648dd3a25362f02fc14942b5edcffbff2b117
Augusto Noronha <augusto2112@me.com>
2023-03-30 14:01:36 -0700

    [clang] Add "transparent_stepping" attribute
    The `transparent_stepping` attribute is intended as a hint for
    debuggers that this function itself is not interesting, but it calls a
    function that might be.  So, when stepping in arrives at a function
    with this attribute, debuggers should transparently step-in through it
    into the functions called by the annotated function (but not by
    subsequent calls made by those functions), stopping at the first one its
    normal rules for whether to stop says to stop at - or stepping out again
    if none qualify. Also, when stepping out arrives at a function with this
    attribute, the debugger should continue stepping out to its caller.
    
    Differential Revision: https://reviews.llvm.org/D146595

--- llvm/lib/CodeGen/AsmPrinter/DwarfUnit.cpp
+++ llvm/lib/CodeGen/AsmPrinter/DwarfUnit.cpp
@@ -1345,6 +1345,9 @@ void DwarfUnit::applySubprogramAttributes(const DISubprogram *SP, DIE &SPDie,
   if (!SP->getTargetFuncName().empty())
     addString(SPDie, dwarf::DW_AT_trampoline, SP->getTargetFuncName());
 
+  if (SP->getIsTransparentStepping())
+    addFlag(SPDie, dwarf::DW_AT_trampoline);
+
   if (DD->getDwarfVersion() >= 5 && SP->isDeleted())
     addFlag(SPDie, dwarf::DW_AT_deleted);
 }
